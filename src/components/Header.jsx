7622f2e796011d991cbcd6c2a662f40a47764aae
hardikk258 <hardiksingh241@gmail.com>
2024-08-09 12:42:55 +0530

    changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,11 +6,11 @@ const Header = () => {
   return (
     <div className='navbar' >
         <div className="logo">
-            <h1>CryptoVerse </h1>
+            <h1>CryptoPulse</h1>
             <FaEthereum color='orange' size={"25"} /> 
         </div>
       <ul>
-        <li> <Link to='/' >Home</Link> </li>
+        <li> <Link to='/'>Home</Link> </li>
         <li> <Link to='/coins'>Coins</Link></li>
       </ul>
     </div>
